504d80318d06a8f8ad3e447503f0a548b3d0fabb
anokabb <anouarkabbouri6@gmail.com>
2021-10-28 18:38:36 +0100

    adding post  get to api

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import serializers
+from rest_framework import status
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View."""
+
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """returns a list of apiview features."""
+
+        an_apiview = ['Uses HTTP methods as function (get,post,put,delete)']
+
+        return Response({'message': 'HEllo!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
